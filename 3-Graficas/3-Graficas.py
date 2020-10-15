52d70ee6772ba5c3119a7102d0f15e107d7f9e22
DESKTOP-REDASDA\admin <javigta97@gmail.com>
2020-10-15 17:27:56 +0200

    Parece que funciona para una sola gráfica. Falta integrar el resto de gráficas

---
+++ 3-Graficas/3-Graficas.py
@@ -0,0 +1,127 @@
+import serial;
+import time;
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+#---------------- FUNCION PARA LEER Y GUARDAR VALORES ---------------- 
+def cogerValoresyEscribirlos():   
+    val = SerialArduino.readline().decode('ascii')
+    val = val.rstrip("\r\n")
+   # print(val)        
+    val += "\r"
+    
+    with open("datosAcelerometro.csv","a") as f: 
+        f.write(val)
+    #with open("datosAcelerometro.txt","a") as f: 
+        #f.write(val) 
+
+
+ #---------------- FUNCION PARA PLOTEAR ----------------        
+def plotear(i, temp, AcelX, AcelY, AcelZ, Anorma):
+    tiempoInicio = time.time()
+    tiempoActual = tiempoInicio
+    
+    file = open("datosAcelerometro.csv","w")
+    file.write("AcelX;AcelY;AcelZ;Norma;\r")
+    file.close()
+    
+    while ((tiempoActual - tiempoInicio)< tiempoTomadeDatos):   
+        #Nos guardamos los datos durante tiempoTomadeDatos segundos
+        cogerValoresyEscribirlos()            
+        tiempoActual = time.time()
+        
+        if ((tiempoActual - tiempoInicio) >= tiempoTomadeDatos):
+            #Actualizamos contador que nos sirve para ir iterando las gráficas:
+            i=i+1
+            #Cargamos los últimos datos en la variable df 
+            df = pd.read_csv("datosAcelerometro.csv", sep=';')
+            #Calculamos medias y varianzas
+            AcelX.append(df["AcelX"].mean())
+            #df["AcelX"].var()
+            AcelY.append(df["AcelY"].mean())
+            AcelZ.append(df["AcelZ"].mean())
+            
+            temp.append(tiempoActual)
+            
+            #Dibujamos 
+            ax.clear()
+            ax.plot(temp, AcelX, label="AcelX media (en g)")
+            ax.plot(temp, AcelY, label="AcelY media (en g)")
+            
+            # Format plot
+            #plt.xticks(rotation=45, ha='right')
+            #plt.subplots_adjust(bottom=0.30)
+            plt.title('Aceleración en X')
+            plt.ylabel('Valor (g)')
+            plt.xlabel('tiempo')
+            plt.legend()
+            #plt.axis([1, None, 0, 1.1]) #Use for arbitrary number of trials
+            #plt.axis([1, 100, 0, 1.1]) #Use for 100 trial demo
+        
+        
+    
+    
+
+#------------------- DECLARACION DEL PUERTO SERIE -------------------        
+#Definimos el puerto serie:
+SerialArduino = serial.Serial('COM3',115200)
+#Damos un tiempo (1seg) al puerto para que se conecte:
+time.sleep(1)
+
+#------------------- PROCESO DE CALIBRACION DEL SENSOR ------------------- 
+aux = "a"
+print("\n-------------- CALIBRACION --------------\n")
+print("Para calibrar mandar algo por el Puerto Serie:\n")
+print("IMPORTANTE!! Debe estar en reposo\n")
+input()
+SerialArduino.write(aux.encode('ascii'))
+print("-------------- Sensor calibrado --------------\n")
+
+
+#---------------- COMIENZO DE LA CAPTURA DE DATOS --------------------------
+
+#Por si acaso tenemos que dejar un tiempo entre la calibracion y la captura de
+#los datos, le dejamos al usuario que lo indique por la consola cuando quiere
+#empezar a capturar datos:
+input("Manda algo por la consola si quieres empezar a tomar datos\n\n")
+SerialArduino.write(aux.encode('ascii'))
+
+#---------------------- SET-UP DE LAS GRÁFICAS ----------------------------
+# Figura:
+fig, ax = plt.subplots()
+# Inicilizamos las líneas que se plotean:
+#   Medias:
+AcelXmean = []; AcelYmean = []; AcelZmean = []; Anormamean = []
+#   Desviaciones típicas:
+AcelXstd  = []; AcelYstd  = []; AcelZstd  = []; Anormastd  = []
+# Eje X común (el tiempo)
+temp    = []
+
+# Un contador, útil para la gráfica (nos servirá para actualizar temp)
+i=0
+
+# Borramos los contenidos del fichero de texto (si es que había contenidos)
+# y añadimos encabezados:
+file = open("datosAcelerometro.csv","w")
+#file.write("AcelX;AcelY;AcelZ;Norma\r")
+file.close()
+file = open("datosAcelerometro.txt","w")
+#file.write("AcelX;AcelY;AcelZ;Norma\r")
+file.close()
+
+# Quitamos los contenidos del buffer del puerto Serie, para que por ejemplo no
+# no guarde el mensaje de reset del propio ESP32
+SerialArduino.reset_input_buffer()
+
+#Tiempos de referencia:
+tiempoCero = time.time()
+tiempoTomadeDatos = 5.0 #segundos
+
+#Capturamos datos durante (tiempoTomadeDatos) segundos:
+# fargs = (temp, AcelXmean, AcelYmean, AcelZmean, Anormamean),        
+ani = animation.FuncAnimation(fig, plotear,  interval=0)
+plt.show()
+ 
+#SerialArduino.close()    
---
+++ 3-Graficas/3-Graficas/3-Graficas.ino
@@ -0,0 +1,148 @@
+#include <MPU9250_asukiaaa.h>
+
+//LED:
+const int ledPIN = 17;
+
+// IMU:
+#ifdef _ESP32_HAL_I2C_H_
+#define SDA_PIN 21
+#define SCL_PIN 22
+#endif
+
+MPU9250_asukiaaa mySensor;
+float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
+float offsetX, offsetY, offsetZ, modulo2;
+float aXtotal = 0.0, aYtotal = 0.0, aZtotal = 0.0;
+
+//INTERRUPCION:
+//Variable para detectar interrupción, y contador:
+volatile int interruptCounter;
+int cont = 0;
+
+//Creamos un puntero que represente a un timer 
+hw_timer_t * timer = NULL;
+
+//Creamos una variable de tipo portMUX_type que nos permita sincronizar la variable volatile
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+//Interrupcion:
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux); 
+}
+
+void setup() {
+  Serial.begin(115200);
+  //while(!Serial);
+  delay(1000); //Para que le de tiempo al puerto serie a abrirse
+
+  // SET-UP del IMU:
+  #ifdef _ESP32_HAL_I2C_H_ // For ESP32
+    Wire.begin(SDA_PIN, SCL_PIN);
+    mySensor.setWire(&Wire);
+  #endif
+
+  mySensor.beginAccel();
+
+  //Esperamos a que el usuario indique que se inicie la calibración:
+  while (!Serial.available()){
+  }
+  //Esperamos a que se haya transmitido el mensaje y no queden caracteres por tanto en el buffer:
+  while(Serial.available() > 0) {
+    char t = Serial.read();
+  }
+
+  if (mySensor.accelUpdate() == 0) {
+    offsetX = mySensor.accelX();
+    offsetY = mySensor.accelY();
+    aZ = mySensor.accelZ();
+  } else {
+    Serial.println("Cannot read accel values");
+  }
+
+  //En la siguiente líne asumimos que el oofset en el eje Z NO es suficiente como para cambiar el signo en la medición:
+  if (aZ > 0){
+    offsetZ = aZ - 1.0;
+  }
+  else {
+    offsetZ = aZ + 1.0;
+  }
+
+  //Esperamos a que el usuario indique que se inicie la captura de datos:
+  while (!Serial.available()){
+  }
+
+  // SET-UP del timer:
+  timer = timerBegin(0, 80, true); //Por orden: asociamos el timer al canal 0, prescaler de 80 -> freq=80/80=1 MHz, y el conteo es en sentido ascendente.
+  timerAttachInterrupt(timer, &onTimer, true); //Asignamos el timer a la interrip. onTimer, y la interrupción se genera tras producirse el flanco de timer correspondiente. 
+  timerAlarmWrite(timer, 100000, true); //Interrupcion = 100.000 conteos (100 ms) e indicamos que cada vez que llegue a ese valor, el conteo se renueve.
+  timerAlarmEnable(timer); //activamos la interrupción.
+
+  // SET-UP del LED:
+  pinMode(ledPIN , OUTPUT);
+
+  portENTER_CRITICAL(&timerMux);
+  interruptCounter=0;
+  portEXIT_CRITICAL(&timerMux);
+  
+}
+
+void loop() {
+
+  //LEEMOS sensor si han pasado 200 ms:
+  if (interruptCounter > 0) {
+
+    //Inicializamos de nuevo la variable, para detectar de nuevo la itnerrupción en el if
+    portENTER_CRITICAL(&timerMux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timerMux);
+
+    //Actualizamos el contador (para ver si hemos llegado al segundo):
+    ++cont;
+    
+    //Leemos address del sensor, en caso de que quisieramos lo podemos sacar tambien por la UART:
+    /*
+    uint8_t sensorId;
+    if (mySensor.readId(&sensorId) == 0) {
+      Serial.println("sensorId: " + String(sensorId));
+    } 
+    else {
+      Serial.println("Cannot read sensorId");
+    }*/
+
+    //Leemos aceleraciones y actualizamos los sumatorios:
+    if (mySensor.accelUpdate() == 0) {
+      aX = mySensor.accelX() - offsetX;       
+      aY = mySensor.accelY() - offsetY; 
+      aZ = mySensor.accelZ() - offsetZ; 
+      //aSqrt = mySensor.accelSqrt();
+    } 
+    else {
+      Serial.println("Cannot read accel values");
+    }    
+
+    if (cont == 2){
+      digitalWrite(ledPIN , LOW);
+    }
+
+    Serial.print(aX);
+    Serial.print(";");
+    Serial.print(aY);
+    Serial.print(";");
+    Serial.print(aZ);
+    Serial.print(";");
+    modulo2 = aX*aX + aY*aY + aZ*aZ;
+    Serial.print(sqrt(modulo2));
+    Serial.println(";");
+
+    
+    
+    //Si ha pasado 1 segundo, enviamos la media de las acelarciones durante ese segundo
+    if (cont==10) {
+      digitalWrite(ledPIN , HIGH);
+      cont = 0;
+    }
+  }
+  
+}
